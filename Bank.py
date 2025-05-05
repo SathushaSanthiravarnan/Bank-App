2c04bd85614e71a01cab407d780090df60f03314
Sathusha Santhiravarnan <UT010651@unicomtic.com>
2025-05-05 13:24:02 +0530

    first commit

---
+++ Bank.py
@@ -0,0 +1,8 @@
+def get_customer_info()
+    name = input("Enter customer's name :")
+    age = input("Enter customer's age :")
+    birthdate = input("Enter costomer's birthdate")
+    address = input("Enter customer's address :")
+    NIC = input("Enter Customer's NIC :")
+    status = input("Enter customer's status")
+    
